f028d49d0bc2fc999ec939a3a6e69dfcb7748f66
EatingAndy <yuyangh@nycstudents.net>
2025-05-06 09:25:31 -0400

    cooked

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask(__name__)
 
+@app.route("/")
 def index():
     response = requests.get("https://completecriminalchecks.com/Developers/limit=100")
     data = response.json()
@@ -24,3 +25,17 @@ def index():
 def criminal_detail(id):
     response = requests.get(f"https://completecriminalchecks.com/Developers/{id}")
     data = response.json
+    height = data.get('height')
+    weight = data.get('weight')
+    name = data.get('name').capitalize()
+    image_url = f"https://completecriminalchecks.com/Developers/{id}.png"
+
+    return render_template("criminals.html," criminal={
+        'name': name,
+        'id': id,
+        'image': image_url
+        'height': height,
+        'weight': weight
+    })
+if __name__ == '__main__':
+    app.run(debug=True)
