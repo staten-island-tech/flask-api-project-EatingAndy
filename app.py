f424e0f179273394d6c1056255e405da6c67b0c1
EatingAndy <yuyangh@nycstudents.net>
2025-05-08 09:24:19 -0400

    genshin

--- app.py
+++ app.py
@@ -1,43 +1,12 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__)
+# Example: Get information about the character Albedo
+url = "https://genshin.jmp.blue/characters/Lisa?lang=en"
 
-@app.route("/")
-def index():
-    response = requests.get("https://completecriminalchecks.com/Developers/limit=100")
-    print(response.text)
+response = requests.get(url)
+
+if response.status_code == 200:
     data = response.json()
-    criminal_list = data['results']
-    criminals = []
-
-    for criminal in criminal_list:
-        url = criminal['url']
-        parts = url.strip("/").split("/")
-        id = parts[-1]
-        image_url = image_url = f"https://completecriminalchecks.com/Developers/{id}.png"
-        criminals.append({
-            'name' : criminal['name'].capitalize(),
-            'id' : id,
-            'image' : image_url
-        })
-    return render_template("index.html", criminals = criminals)
-
-@app.route("/criminal/<id>")
-def criminal_detail(id):
-    response = requests.get(f"https://completecriminalchecks.com/Developers/{id}")
-    data = response.json()
-    height = data.get('height')
-    weight = data.get('weight')
-    name = data.get('name').capitalize()
-    image_url = f"https://completecriminalchecks.com/Developers/{id}.png"
-
-    return render_template("criminals.html", criminal={
-        'name': name,
-        'id': id,
-        'image': image_url,
-        'height': height,
-        'weight': weight
-    })
-if __name__ == '__main__':
-    app.run(debug=True)
+    print("Character Info:")
+    print(data)
